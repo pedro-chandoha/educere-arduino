8d78a4c4b3c9bcc5c6077ce277a7d8858f0fa474
pedro-chandoha <160602791+pedro-chandoha@users.noreply.github.com>
2024-09-19 16:38:09 -0300

    ADD poten4

--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@ Servo myservo4;
 int poten1 = 0;
 int poten2 = 1;
 int poten3 = 2;
+int poten4 = 3;
 int val1;
 int val2;
 int val3;
+int val4;
 
 void setup() {
   Serial.begin (9600);
@@ -70,13 +72,15 @@ void loop() {
   delay(500);
    val1 = analogRead(poten1);
   val2 = analogRead(poten2);
-  val3 = analogRead(poten3);// reads the value of the potentiometer (value between 0 and 1023)
+  val3 = analogRead(poten3);
+  val4 = analogRead(poten4);
   val1 = map(val1, 0, 1080, 0, 180);
   val2 = map(val2, 0, 1080, 0, 180);
-  val3 = map(val3, 0, 1080, 0, 180);// scale it to use it with the servo (value between 0 and 180)
+  val3 = map(val3, 0, 1080, 0, 180);
+  val4 = mpa(val4, 0, 1080, 0, 180);
   myservo1.write(val1);
   myservo2.write(val1);
   myservo3.write(val2);
-  myservo4.write(val3);
+  myservo4.write(val4);
   delay(tempo);                         
 }
